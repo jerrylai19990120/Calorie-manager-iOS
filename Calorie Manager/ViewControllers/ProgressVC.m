d6d45f275d52e2f6cb5a9a7a21f73a6aa0cce1df
jerry <jerrylai19990120@gmail.com>
2021-04-13 11:26:19 -0400

    tab bar view update

---
+++ Calorie Manager/ViewControllers/ProgressVC.m
@@ -0,0 +1,34 @@
+//
+//  ProgressVC.m
+//  Calorie Manager
+//
+//  Created by Jerry Lai on 2021-04-13.
+//  Copyright © 2021 Jerry Lai. All rights reserved.
+//
+
+#import "ProgressVC.h"
+
+@interface ProgressVC ()
+
+@end
+
+@implementation ProgressVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
