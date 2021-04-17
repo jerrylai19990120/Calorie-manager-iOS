ccd7917f79a66926bf15ab98dfd606928a7d7b6f
jerry <jerrylai19990120@gmail.com>
2021-04-17 17:59:15 -0400

    progress view update

---
+++ Calorie Manager/Views/TaskCell.h
@@ -0,0 +1,17 @@
+//
+//  TaskCell.h
+//  Calorie Manager
+//
+//  Created by Jerry Lai on 2021-04-17.
+//  Copyright © 2021 Jerry Lai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TaskCell : UITableViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
