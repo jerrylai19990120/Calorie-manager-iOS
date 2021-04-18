1a49067225cd80c2675e7a21de31b341e6bcd950
jerry <jerrylai19990120@gmail.com>
2021-04-18 02:20:30 -0400

    minor update

---
+++ Calorie Manager/ViewControllers/AddTaskVC.h
@@ -0,0 +1,17 @@
+//
+//  AddTaskVC.h
+//  Calorie Manager
+//
+//  Created by Jerry Lai on 2021-04-18.
+//  Copyright © 2021 Jerry Lai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AddTaskVC : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Calorie Manager/ViewControllers/AddTaskVC.m
@@ -0,0 +1,24 @@
+//
+//  AddTaskVC.m
+//  Calorie Manager
+//
+//  Created by Jerry Lai on 2021-04-18.
+//  Copyright © 2021 Jerry Lai. All rights reserved.
+//
+
+#import "AddTaskVC.h"
+
+@interface AddTaskVC ()
+
+@end
+
+@implementation AddTaskVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+
+@end
